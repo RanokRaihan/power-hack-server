7e414b2b8c215b6d88df97221f5922a71f2683dd
Ranok Raihan <ranokraihan@gmail.com>
2023-01-30 15:45:07 +0600

    done

--- index.js
+++ index.js
@@ -14,12 +14,7 @@ const authRouter = require("./router/authRouter");
 const errorHandler = require("./middleware/errorHandler");
 //init app
 const app = express();
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
+app.use(cors());
 //config environment variable
 dotenv.config();
 
